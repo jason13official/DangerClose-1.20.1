a06ed6d33f4809fb345bab151cf7b40aa3483861
jason13official <jason13official@gmail.com>
2024-03-29 20:12:06 -0400

    fixed pure ResourceLocation in favor of FireManager reference

--- forge/src/main/java/net/jason13/dangerclose/optional/SoulFired.java
+++ forge/src/main/java/net/jason13/dangerclose/optional/SoulFired.java
@@ -6,10 +6,10 @@ import net.minecraft.world.entity.Entity;
 
 public final class SoulFired {
 	public static void setOnRegularFire(Entity entity, int seconds) {
-		FireManager.setOnFire(entity, seconds, new ResourceLocation(""));
+		FireManager.setOnFire(entity, seconds, FireManager.DEFAULT_FIRE_TYPE);
 	}
 	public static void setOnSoulFire(Entity entity, int seconds) {
-		FireManager.setOnFire(entity, seconds, new ResourceLocation("soul"));
+		FireManager.setOnFire(entity, seconds, FireManager.SOUL_FIRE_TYPE);
 	}
 	
 	public static void setOnTypedFire(Entity entity, int seconds, ResourceLocation fireType) {
